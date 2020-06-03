d8d6a4eca1eb461064274b6c71469cf92cc15c69
A L V A R O <alvarop3000@gmail.com>
2020-06-03 17:42:37 -0500

    fix user_index test

--- spec/views/user_index_spec.rb
+++ spec/views/user_index_spec.rb
@@ -2,16 +2,17 @@ require 'rails_helper'
 
 RSpec.describe 'User Index', type: :system do
   before :each do
-    User.make(email: 'moin@email.com', password: '123456')
+    User.create(name: 'Moin', email: 'moin@email.com', password: '123456')
   end
   context 'after creating a user' do
     it "signs me in" do
       visit new_user_session_path
-      within("#session") do
+      within("#new_user") do
         fill_in 'Email', with: 'moin@email.com'
         fill_in 'Password', with: '123456'
       end
-      click_button 'Sign in'
+      click_button 'Log in'
+      visit users_path
       expect(page).to have_content('Name: Moin')
     end
   end
