d5416babdbd90d4a4c35fea619b0e87a4166f877
A L V A R O <alvarop3000@gmail.com>
2020-06-03 13:12:39 -0500

    add tests for user model and post view

---
+++ spec/views/post_index_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe 'Home Page', type: :system do
+  context "While visiting the home page" do
+    it "page contains 'Sign in'" do
+      visit root_path
+      expect(page).to have_content('Sign in')
+    end
+  end
+end
