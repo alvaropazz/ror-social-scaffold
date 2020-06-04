be42c0c4a7776a5d66742a922359db6e0523fafd
A L V A R O <alvarop3000@gmail.com>
2020-06-04 13:15:42 -0500

    create confirm, decline methods, update user#show view

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,27 +9,27 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-  has_many :friendships
-  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+  has_many :friendships, dependent: :destroy
+  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id', dependent: :destroy
 
   def friends
     friends_array = friendships.map do |friendship|
       friendship.friend if friendship.confirmed
     end + inverse_friendships.map do |friendship|
-            friendship.friend if friendship.confirmed
+            friendship.user if friendship.confirmed
           end
     friends_array.compact
   end
 
   def pending_friends
-    friendships.map do |_frienship|
+    friendships.map do |friendship|
       friendship.friend unless friendship.confirmed
     end .compact
   end
 
   def friend_requests
     inverse_friendships.map do |friendship|
-      friendship.friend unless friendship.confirmed
+      friendship.user unless friendship.confirmed
     end .compact
   end
 
@@ -41,11 +41,17 @@ class User < ApplicationRecord
     friendship.save
   end
 
+  def decline_friend(user)
+    inverse_friendships.where(user_id: user.id).first.destroy
+  end
+
   def friend?(user)
     friends.include?(user)
   end
 
   def send_request(user)
-    friendships.create(friend_id:user.id, confirmed:false)
+    unless pending_friends.include?(user)
+    friendships.create(friend_id: user.id, confirmed:false)
+    end
   end
 end
